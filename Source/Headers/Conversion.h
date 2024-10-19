abb60b944a8b279eb4a4e6e87fa247a5182c9102
HarrisonBraml3y <harrison-bramley@hotmail.com>
2024-10-19 23:02:13 +0100

    19/10/2024

--- Source/Headers/Conversion.h
+++ Source/Headers/Conversion.h
@@ -78,6 +78,53 @@ int PopulateMap(std::filesystem::path ReadFrom, std::map<std::string, std::strin
 	return 0;
 }
 
+//Fill map of updated directories e.g. assets/minecraft/mcpatcher -> assets/minecraft/optifine
+//loop through an old directory, 
+int PopulateDirMap(std::map<std::filesystem::path, std::filesystem::path>& DirMap) {
+	DirMap[""] = "assets\\minecraft\\lang";
+	DirMap["assets\\minecraft\\mcpatcher"] = "assets\\minecraft\\optifine";
+	DirMap["assets\\minecraft\\mcpatcher\\font"] = "assets\\minecraft\\optifine\\font";
+	DirMap["assets\\minecraft\\mcpatcher\\lightmap"] = "assets\\minecraft\\optifine\\lightmap";
+	DirMap["assets\\minecraft\\mcpatcher\\mob"] = "assets\\minecraft\\optifine\\mob";
+	DirMap["assets\\minecraft\\mcpatcher\\sky"] = "assets\\minecraft\\optifine\\sky";
+	DirMap["assets\\minecraft\\mcpatcher\\sky\\sky0"] = "assets\\minecraft\\optifine\\sky\\sky0";
+	DirMap[""] = "assets\\minecraft\\texts";
+	//DirMap["assets\minecraft\textures\entity\cat"] = "assets\minecraft\textures\entity\cat";
+	//DirMap["assets\minecraft\textures\entity\chest"] = "assets\minecraft\textures\entity\chest";
+	//DirMap["assets\minecraft\textures\entity\cow"] = "assets\minecraft\textures\entity\cow";
+	//DirMap["assets\minecraft\textures\entity\creeper"] = "assets\minecraft\textures\entity\creeper";
+	//DirMap["assets\minecraft\textures\entity\endercrystal"] = "assets\minecraft\textures\entity\endercrystal";
+	//DirMap["assets\minecraft\textures\entity\enderdragon"] = "assets\minecraft\textures\entity\enderdragon";
+	//DirMap["assets\minecraft\textures\entity\enderman"] = "assets\minecraft\textures\entity\enderman";
+	//DirMap["assets\minecraft\textures\entity\ghast"] = "assets\minecraft\textures\entity\ghast";
+	//DirMap["assets\minecraft\textures\entity\horse"] = "assets\minecraft\textures\entity\horse";
+	//DirMap["assets\minecraft\textures\entity\pigsheep"] = "assets\minecraft\textures\entity\pigsheep";
+	//DirMap["assets\minecraft\textures\entity\skeleton"] = "assets\minecraft\textures\entity\skeleton";
+	//DirMap["assets\minecraft\textures\entity\slime"] = "assets\minecraft\textures\entity\slime";
+	//DirMap["assets\minecraft\textures\entity\spider"] = "assets\minecraft\textures\entity\spider";
+	//DirMap["assets\minecraft\textures\entity\villager"] = "assets\minecraft\textures\entity\villager";
+	//DirMap["assets\minecraft\textures\entity\wither"] = "assets\minecraft\textures\entity\wither";
+	//DirMap["assets\minecraft\textures\entity\wolf"] = "assets\minecraft\textures\entity\wolf";
+	//DirMap["assets\minecraft\textures\entity\zombie"] = "assets\minecraft\textures\entity\zombie";
+	//DirMap["assets\minecraft\textures\environment"] = "assets\minecraft\textures\environment";
+	//DirMap["assets\minecraft\textures\font"] = "assets\minecraft\textures\font";
+	//DirMap["assets\minecraft\textures\gui"] = "assets\minecraft\textures\gui";
+	//DirMap["assets\minecraft\textures\gui\achivement"] = "assets\minecraft\textures\gui\achivement";
+	//DirMap["assets\minecraft\textures\gui\container"] = "assets\minecraft\textures\gui\container";
+	//DirMap["assets\minecraft\textures\gui\container\creative_inventory"] = "assets\minecraft\textures\gui\container\creative_inventory";
+	DirMap["assets\\minecraft\\textures\\blocks"] = "assets\\minecraft\\textures\\block";
+	DirMap["assets\\minecraft\\textures\\items"] = "assets\\minecraft\\textures\\item";
+	//DirMap["assets\minecraft\textures\map"] = "assets\minecraft\textures\map";
+	//DirMap["assets\minecraft\textures\misc"] = "assets\minecraft\textures\misc";
+	DirMap[""] = "assets\\minecraft\\textures\\mob_effect";
+	//DirMap["assets\minecraft\textures\models"] = "assets\minecraft\textures\models";
+	//DirMap["assets\minecraft\textures\models\armor"] = "assets\minecraft\textures\models\armor";
+	//DirMap["assets\minecraft\textures\painting"] = "assets\minecraft\textures\painting";
+	//DirMap["assets\minecraft\textures\particle"] = "assets\minecraft\textures\particle";
+
+
+}
+
 
 
 int Search() {
@@ -106,10 +153,10 @@ int Rename(std::vector<std::string>& ToRename, std::map<std::string, std::string
 //, after it has been renamed
 	return 0;
 }
-int RenameSingle(std::string& ToRename, std::map<std::string, std::string> Names) {	//Put all to-be-renamed names in vector, match them with first value of Names map, 
-	std::cout << "Rename()" << std::endl;													//Afterwards, rename the full file path to the new file path
+std::string RenameSingle(std::string& ToRename, std::map<std::string, std::string> Names) {	//Put all to-be-renamed names in vector, match them with first value of Names map, 
+	std::cout << "RenameSingle()" << std::endl;													//Afterwards, rename the full file path to the new file path
 	std::cout << "ToRename size: " << ToRename.size() << std::endl;
-
+	std::cout << "Renaming " << ToRename << std::endl;
 
 		if (Names.find(ToRename) == Names.end()) {
 			std::cout << "No key found" << std::endl;
@@ -123,7 +170,7 @@ int RenameSingle(std::string& ToRename, std::map<std::string, std::string> Names
 
 	//also remember to change the file directory of the original file to the newly created directory
 	//, after it has been renamed
-	return 0;
+	return ToRename;
 }
 
 int FetchNames(std::string Path, std::vector<std::string>& Paths) {	//fetch all files in directory,
@@ -139,46 +186,89 @@ int FetchNames(std::string Path, std::vector<std::string>& Paths) {	//fetch all 
 	return 1;
 }
 
-void IterateFolder(std::filesystem::path Folder, std::filesystem::path NewDir, std::vector<std::string> Names, std::map<std::string, std::string> NamesMap) {
-	
-	MoveFiles(Folder, NewDir, Names, NamesMap); 
 
-	for (auto i : std::filesystem::directory_iterator(Folder)) {
+
+//Doesn't account for files which may be in different directories post-update or renamed directories
+void MoveFiles(std::filesystem::path Master, std::filesystem::path OldDir, std::filesystem::path NewDir, std::vector<std::string>& Names, std::map<std::string, std::string> NamesMap) {	//call this function for each directory in the folder
+	std::cout << Master << std::endl;
+
+	std::filesystem::path OldPath;
+	std::filesystem::path NewPath;
+	//add check if it's not a file but a directory
+	for (auto& Entry : std::filesystem::directory_iterator(OldDir)) {
+		OldPath = Entry.path();
+		std::string Temp = Entry.path().string();
+		std::string FileName = Temp.substr(Temp.find_last_of("/\\") + 1);
+		std::cout << "Temp: " << FileName << std::endl;
+
+		RenameSingle(FileName, NamesMap);
+		std::cout << "OldPath:" << OldPath << std::endl;
+		std::cout << "OldPath parent:" << OldPath.parent_path() << std::endl;
+		std::cout << "Master:" << Master << std::endl;
+		
+		std::filesystem::path RelativePath = std::filesystem::relative(OldPath.parent_path(), Master);	//if old and new relative path is the same, continue, if different then try and fix it with DirMap
+		std::cout << "NewDir:" << NewDir << std::endl;
+		std::cout << "RelativePath:" << RelativePath << std::endl;
+		std::cout << "FileName:" << FileName << std::endl;
+
+		NewPath = NewDir / RelativePath / FileName;
+		Names.push_back(Entry.path().filename().string());
+
+		std::cout << "OldPath: " << OldPath << std::endl;
+		std::cout << "NewPath: " << NewPath << std::endl;
+		std::filesystem::rename(OldPath, NewPath);
+		std::cout << OldPath << " Changed to: " << NewPath << std::endl;
+	}
+	return;
+}
+
+//void MoveFiles(std::filesystem::path OldDir, std::filesystem::path NewDir, std::vector<std::string>& Names, std::map<std::string, std::string> NamesMap) {	//call this function for each directory in the folder
+//	std::string OldPath;
+//	std::filesystem::path NewPath;
+//	std::filesystem::path Master = OldDir;
+//	//add check if it's not a file but a directory
+//	for (auto& Entry : std::filesystem::directory_iterator(OldDir)) {
+//		OldPath = Entry.path().string();
+//		std::string FileName = OldPath.substr(OldPath.find_last_of("/\\") + 1);
+//		std::cout << "Temp: " << FileName << std::endl;
+//
+//		RenameSingle(FileName, NamesMap);
+//		std::cout << "OldDir:" << OldDir << std::endl;
+//		std::cout << "OldDir parent:" << OldDir.parent_path() << std::endl;
+//		std::cout << "Master:" << Master << std::endl;
+//		std::filesystem::path RelativePath = std::filesystem::relative(OldDir.parent_path(), Master);
+//		std::cout << "NewDir:" << NewDir << std::endl;
+//		std::cout << "RelativePath:" << RelativePath << std::endl;
+//		std::cout << "FileName:" << FileName << std::endl;
+//
+//		NewPath = NewDir / RelativePath / FileName;
+//		Names.push_back(Entry.path().filename().string());
+//
+//		std::cout << "NewPath: " << NewPath << std::endl;
+//		std::filesystem::rename(OldPath, NewPath);
+//		std::cout << OldPath << " Changed to: " << NewPath << std::endl;
+//	}
+//	//reconstruct path with renamed names
+//
+//	return;
+//}
+
+void IterateFolder(std::filesystem::path Master, std::filesystem::path Folder, std::filesystem::path NewDir, std::vector<std::string> Names, std::map<std::string, std::string> NamesMap) {
+	std::cout << Folder << std::endl;
+	for (auto& i : std::filesystem::directory_iterator(Folder)) {
 		if (!i.is_directory()) {
-			
+			MoveFiles(Master, Folder, NewDir, Names, NamesMap);
 		}
 		else {
 			std::cout << i << std::endl;
 			//std::filesystem::path Entry = i;
 			//MoveFiles(Entry, NewDir, Names, NamesMap);
-			IterateFolder(i, NewDir, Names, NamesMap);
+			IterateFolder(Master, i, NewDir, Names, NamesMap);
 		}
 	}
 }
 
-void MoveFiles(std::filesystem::path OldDir, std::filesystem::path NewDir, std::vector<std::string>& Names, std::map<std::string, std::string> NamesMap){	//call this function for each directory in the folder
-	std::string OldPath;
-	std::filesystem::path NewPath;
-	std::string TempPath;
 
-	
-
-	for (auto& Entry : std::filesystem::directory_iterator(OldDir)) {
-		OldPath = Entry.path().string();
-		std::string Temp = OldPath.substr(OldPath.find_last_of("/\\") + 1);
-		std::cout << "Temp: " << Temp << std::endl;	
-		RenameSingle(Temp, NamesMap);
-		NewPath = NewDir / Temp;
-		Names.push_back(Entry.path().filename().string());
-
-		std::cout << "NewPath: " << NewPath << std::endl;
-		std::filesystem::rename(OldPath, NewPath);
-		std::cout << OldPath << " Changed to: " << NewPath << std::endl;
-	}
-	//reconstruct path with renamed names
-
-	return;
-}
 
 std::filesystem::path CreateDir(std::string OldDir) {
 	std::filesystem::path NewDir = OldDir + "NEW";
@@ -259,3 +349,10 @@ std::filesystem::path CreateDir(std::string OldDir) {
 
 
 //SpriteSheetSplitter
+
+
+
+
+
+
+
--- Source/Source.cpp
+++ Source/Source.cpp
@@ -6,33 +6,29 @@
 
 
 
-int main(int argc, char* argv[]) {		//when complete change to int main(int argc, char *argv[]) for providing folder names in command line
-
-
-
+int main(int argc, char* argv[]) {	
 	std::map<std::string, std::string> BlocksMap;
 	std::map<std::string, std::string> ItemsMap;
 	std::map<std::string, std::string> NewMap;
 
 	std::filesystem::path BlocksSheetPath = "C:\\Users\\harri\\Desktop\\BlocksSheet.csv";
 	std::filesystem::path ItemsSheetPath = "C:\\Users\\harri\\Desktop\\ItemsSheet.csv";
-	//std::ifstream BlocksSheet = "C:\\Users\\harri\\Desktop\\BlocksMap";
-	//BlocksSheet.open("C:\\Users\\harri\\Desktop\\BlocksMap");
 
 	std::vector<std::string> ToRename;
 	std::string OldDir = "C:\\Users\\harri\\Desktop\\Eum x Faith Mashup - Copy";
 	std::filesystem::path NewDir;
 
-	std::filesystem::directory_iterator DirIterator;
-
-	
 	NewDir = CreateDir(argv[1]);
-
+	std::cout << "NewDir: " << NewDir << std::endl;
 
 	//Loop through main subdirectories
 	PopulateMap(BlocksSheetPath, NewMap);	//figure out how to do this efficiently, re-using the same map but identifying which sheet to pull data from
 
-	IterateFolder(argv[1], NewDir, ToRename, NewMap);
+	//IterateFolder(argv[1], argv[1], NewDir, ToRename, NewMap);	//NewDir needs to be corresponding directory of the original subdir
+	IterateFolder(argv[1], , NewDir, ToRename, NewMap);
+
+
+
 
 //	for (auto i : std::filesystem::directory_iterator(argv[1])) {
 //		if (!i.is_directory()) {
@@ -52,11 +48,6 @@ int main(int argc, char* argv[]) {		//when complete change to int main(int argc,
 	//MoveFiles("C:\\Users\\harri\\Desktop\\Eum x Faith Mashup\\assets\\minecraft\\textures\\blocks", "C:\\Users\\harri\\Desktop\\TestPackNEW\\assets\\minecraft\\textures\\block", ToRename, BlocksMap);
 	//FetchNames("C:\\Users\\harri\\Desktop\\Eum x Faith Mashup\\assets\\minecraft\\textures\\blocks", ToRename);
 	//Rename(ToRename, BlocksMap);
-
-
 	
-
 	return 0;
 }
-
-//try run the main functions in a loop (populate map, iterate folders etc.)
